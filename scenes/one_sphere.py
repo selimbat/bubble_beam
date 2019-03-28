c964e317a8c56b28b98622ac1de76dd7595a9850
selimbat <selim.tirellil@student.ecp.fr>
2019-03-28 21:13:26 +0100

    adding image exemples

--- scenes/one_sphere.py
+++ scenes/one_sphere.py
@@ -10,21 +10,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 this_scene = Scene()
-some_material = Material(np.array([0.,0.,1.]),0.5,0.7,0.1,0.3,0.4)
-red_material = Material(np.array([1.,0.,0.]),1.,1.,1.,1.,1.)
+some_material = Material(np.array([106.,102.,163.])/255,0.5,0.7,0.1,0.3,0.4)
+red_material = Material(np.array([255.,151.,112.])/255,0.8,0.4,0.2,0.4,0.7)
 
-white_light = Spotlight(np.array([1.,1.,0.]),np.array([1.,1.,1.]))
+light1 = Spotlight(np.array([1.,1.,0.]),np.array([252.,236.,201.])/255)
 #red_light = Spotlight(np.array([-1,2,1]),np.array([1,1,0]))
 #other_light = Spotlight(np.array([0,-1,1.5]),np.array([1,0,1]))
 blue_sphere = Sphere(np.array([0.,0.,3.]), 0.8, some_material)
-red_sphere = Sphere(np.array([0.4,0.4,1.8]), 0.2, red_material)
+red_sphere = Sphere(np.array([0.4,0.4,1.8]), 0.1, red_material)
 
 this_scene.add_object(blue_sphere)
 this_scene.add_object(red_sphere)
-this_scene.add_light(white_light)
+this_scene.add_light(light1)
 #this_scene.add_light(red_light)
 #this_scene.add_light(other_light)
 
 this_camera = Camera(1500,1500,2.2)
 
-plt.imsave('first reflection.png', raytracer_render(this_camera, this_scene))
+plt.imsave('shadow exemple.png', raytracer_render(this_camera, this_scene))
